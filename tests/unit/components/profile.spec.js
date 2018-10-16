9f0100dc993b830d388c948d4a72369bbde442d6
Cash Wu <cash@cashwu.com>
2018-10-16 17:05:39 +0800

    profile test

--- tests/unit/components/profile.spec.js
+++ tests/unit/components/profile.spec.js
@@ -3,14 +3,19 @@ import {shallowMount} from '@vue/test-utils'
 import Api from '@/api'
 
 describe('profile', () => {
+
     function wrapper() {
+        Api.getProfile = jest.fn().mockReturnValue(null);
         return shallowMount(profile)
     }
 
     it('save', () => {
         var vm = wrapper().vm;
-        let updateProfile = Api.updateProfile = jest.fn()
+        // Api.updateProfile = jest.fn()
+        let saveProfile
+        Api.updateProfile = profile => { saveProfile = profile }
         vm.save()
-        expect(updateProfile).toHaveBeenCalledWith(vm.profile)
+        // expect(Api.updateProfile).toHaveBeenCalledWith(vm.profile)
+        expect(saveProfile).toEqual(vm.profile)
     })
 })
