df7950a4b276c42301545ede8491a1fa3530704e
Raymond <ddt_df@hotmail.com>
2018-10-16 10:11:15 +0800

    add more test case

--- tests/unit/domain/fizzBuzz.spec.js
+++ tests/unit/domain/fizzBuzz.spec.js
@@ -1,26 +1,30 @@
 import {
     FizzBuzz,
-  } from '../../../src/domain/fizzBuzz'
+  } from '@/domain/fizzBuzz'
   
   describe('FizzBuzz', () => {
     let fizzBuzz
     beforeEach(() => {
         fizzBuzz = new FizzBuzz()
     })
-    it('1 should return 1', ()=>{
+    it('normal number should return itself', ()=>{
         expect(fizzBuzz.Show(1)).toEqual('1');
+        expect(fizzBuzz.Show(8)).toEqual('8');
     })
 
-    it('3 should return Fizz', ()=>{
+    it('3 times should return Fizz', ()=>{
         expect(fizzBuzz.Show(3)).toEqual('Fizz');
+        expect(fizzBuzz.Show(9)).toEqual('Fizz');
     })
 
-    it('5 should return Buzz', ()=>{
+    it('5 times should return Buzz', ()=>{
         expect(fizzBuzz.Show(5)).toEqual('Buzz');
+        expect(fizzBuzz.Show(20)).toEqual('Buzz');
     })
 
-    it('15 should return FizzBuzz', ()=>{
+    it('15 times should return FizzBuzz', ()=>{
         expect(fizzBuzz.Show(15)).toEqual('FizzBuzz');
+        expect(fizzBuzz.Show(60)).toEqual('FizzBuzz');
     })
     
 })
