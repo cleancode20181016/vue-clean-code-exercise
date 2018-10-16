c750da7719fc5ca44445685124142fddf9b5acb2
redeyes2015 <redeyes2015@gmail.com>
2018-10-16 15:19:13 +0800

    Test Profile should use getProfile from api

--- tests/unit/components/profile.spec.js
+++ tests/unit/components/profile.spec.js
@@ -2,19 +2,38 @@ import Profile from '@/views/Profile.vue'
 import Api from '@/api.js'
 import {shallowMount} from '@vue/test-utils'
 
+//jest.mock('@/api.js')
+
 describe('Profile', () => {
+    const myProfile = {
+        name: 'Ryan',
+        birthday: {
+          year: 1984,
+          month: 10,
+          day: 9,
+        }
+    };
+
+    it('should call api.getProfile on beforeMount', () => {
+        Api.getProfile = jest.fn()
+          .mockReturnValue(myProfile)
+        const wrapper = shallowMount(Profile);
+
+        expect(wrapper.vm.profile).toEqual(myProfile);
+    })
     it('should call api.update on save', () => {
         Api.updateProfile = jest.fn();
         const wrapper = shallowMount(Profile);
 
+        Object.assign(wrapper.vm.$data.profile, myProfile);
         wrapper.vm.save();
 
         expect(Api.updateProfile).toHaveBeenCalledWith({
-            name: '',
+            name: 'Ryan',
             birthday: {
-                year: 1980,
-                month: 1,
-                day: 1,
+              year: 1984,
+              month: 10,
+              day: 9,
             }
         })
     })
