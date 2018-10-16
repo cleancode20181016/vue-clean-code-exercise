d117975f76f82ccea04497085aed396097be0ae3
Cash Wu <cash@cashwu.com>
2018-10-16 21:47:17 +0800

    refactor to remove duplication of calculating amount within a budget

--- src/domain/budget.js
+++ src/domain/budget.js
@@ -1,46 +1,44 @@
 import moment from 'moment'
 
 export class Budget {
-  budgets = {}
+    budgets = {}
 
-  query(startDate, endDate) {
+    query(startDate, endDate) {
 
-    const momentStartDate = moment(startDate, 'YYYY-MM-DD')
-    const momentEndDate = moment(endDate, 'YYYY-MM-DD')
+        const momentStartDate = moment(startDate, 'YYYY-MM-DD')
+        const momentEndDate = moment(endDate, 'YYYY-MM-DD')
 
-    if (momentStartDate.isSame(momentEndDate, 'month')) {
-      const diffDays = momentEndDate.diff(momentStartDate, 'days') + 1
-      const budget = ((this.budgets[momentStartDate.format("YYYY-MM")] || 0) / momentStartDate.daysInMonth()) * diffDays
-      return budget
-    } else {
-      let budget = 0
+        if (momentStartDate.isSame(momentEndDate, 'month')) {
+            return this._getAmountOfPeriod(momentStartDate, momentEndDate)
+        } else {
+            let budget = 0
 
-      // start month
-      const numberOfDaysInStartMonth = moment(momentStartDate).endOf('month').diff(momentStartDate, 'days') + 1
-      const amountDaysFirst = momentStartDate.daysInMonth()
-      const firstMonthBudget = this.budgets[momentStartDate.format("YYYY-MM")] || 0
-      const totalBudgetFirstMonth = numberOfDaysInStartMonth * (firstMonthBudget / amountDaysFirst)
-      budget += totalBudgetFirstMonth
+            // start month
+            budget += this._getAmountOfPeriod(momentStartDate, moment(momentStartDate).endOf('month'))
 
-      // months in between
-      const monthDiff = momentEndDate.diff(momentStartDate, 'months') - 1
-      for (let month = 1; month <= monthDiff; month++) {
-        const monthString = moment(momentStartDate)
-          .add(month, 'month')
-          .format('YYYY-MM')
-        const budgetThisMonth = this.budgets[monthString] || 0
-        budget += budgetThisMonth
-      }
+            // months in between
+            const monthDiff = momentEndDate.diff(momentStartDate, 'months') - 1
+            for (let month = 1; month <= monthDiff; month++) {
+                const monthString = moment(momentStartDate)
+                    .add(month, 'month')
+                    .format('YYYY-MM')
+                const budgetThisMonth = this.budgets[monthString] || 0
+                budget += budgetThisMonth
+            }
 
-      // end month
-      const numberOfDaysInLastMonth = momentEndDate.diff(moment(momentEndDate).startOf('month'), 'days') + 1
-      const amountDaysLast = momentEndDate.daysInMonth()
-      const lastMonthBudget = this.budgets[momentEndDate.format("YYYY-MM")] || 0
-      const totalBudgetLastMonth = numberOfDaysInLastMonth * (lastMonthBudget / amountDaysLast)
-      budget += totalBudgetLastMonth
-      return budget
+            // end month
+            budget += this._getAmountOfPeriod(moment(momentEndDate).startOf('month'), momentEndDate)
+            return budget
+        }
     }
-  }
+
+    _getAmountOfPeriod(momentStartDate, momentEndDate) {
+        const numberOfDaysInMonth = momentEndDate.diff(momentStartDate, 'days') + 1
+        let amountDays = momentStartDate.daysInMonth();
+        let momthBudget = this.budgets[momentStartDate.format("YYYY-MM")] || 0;
+        const budget = numberOfDaysInMonth * ((momthBudget) / amountDays)
+        return budget;
+    }
 }
 
 
