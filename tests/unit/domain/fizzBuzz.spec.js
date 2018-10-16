233f2f78d593d9c00430bbd86711457c12425c47
Raymond <ddt_df@hotmail.com>
2018-10-16 10:02:28 +0800

    change case name

--- tests/unit/domain/fizzBuzz.spec.js
+++ tests/unit/domain/fizzBuzz.spec.js
@@ -7,19 +7,19 @@ import {
     beforeEach(() => {
         fizzBuzz = new FizzBuzz()
     })
-    it('should return 1', ()=>{
+    it('1 should return 1', ()=>{
         expect(fizzBuzz.Show(1)).toEqual('1');
     })
 
-    it('should return Fizz', ()=>{
+    it('3 should return Fizz', ()=>{
         expect(fizzBuzz.Show(3)).toEqual('Fizz');
     })
 
-    it('should return Buzz', ()=>{
+    it('5 should return Buzz', ()=>{
         expect(fizzBuzz.Show(5)).toEqual('Buzz');
     })
 
-    it('should return FizzBuzz', ()=>{
+    it('15 should return FizzBuzz', ()=>{
         expect(fizzBuzz.Show(15)).toEqual('FizzBuzz');
     })
     
