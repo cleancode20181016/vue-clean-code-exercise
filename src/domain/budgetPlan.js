c4eb993dde55a0611ec02eb7842a433ef3ac2980
Cash Wu <cash@cashwu.com>
2018-10-16 22:20:07 +0800

    extract budget class

--- src/domain/budgetPlan.js
+++ src/domain/budgetPlan.js
@@ -11,6 +11,12 @@ class Period {
     }
 }
 
+class Budget{
+    constructor(month, amount){
+        this.amount = amount || 0
+    }
+}
+
 export class BudgetPlan {
     budgets = {}
 
@@ -21,12 +27,17 @@ export class BudgetPlan {
 
     _query(period) {
         if (period.start.isSame(period.end, 'month')) {
-            return this._getAmountOfPeriod(period)
+            return this._getAmountOfPeriod(period,
+                new Budget(period.start.format("YYYY-MM"),
+                    this.budgets[period.start.format("YYYY-MM")]))
         } else {
             let budget = 0
 
             // start month
-            budget += this._getAmountOfPeriod(new Period(period.start, moment(period.start).endOf('month')))
+            budget += this._getAmountOfPeriod(
+                new Period(period.start, moment(period.start).endOf('month')),
+                new Budget(period.start.format("YYYY-MM"),
+                    this.budgets[period.start.format("YYYY-MM")]))
 
             // months in between
             const monthDiff = period.end.diff(period.start, 'months') - 1
@@ -38,15 +49,18 @@ export class BudgetPlan {
             }
 
             // end month
-            budget += this._getAmountOfPeriod(new Period(moment(period.end).startOf('month'), period.end))
+            budget += this._getAmountOfPeriod(
+                new Period(moment(period.end).startOf('month'), period.end),
+                new Budget(period.end.format("YYYY-MM"),
+                    this.budgets[period.end.format("YYYY-MM")]))
             return budget
         }
     }
 
-    _getAmountOfPeriod(period) {
+    _getAmountOfPeriod(period, budget) {
         const diffDays = period.dayCount();
         const dayCountOfBudget = period.start.daysInMonth();
-        const amountOfBudget = this.budgets[period.start.format("YYYY-MM")] || 0;
+        const amountOfBudget = budget.amount;
         return amountOfBudget / dayCountOfBudget * diffDays;
     }
 }
