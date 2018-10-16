714c9e409934254b13e32ae2995beb74d6ea7d5f
Cash Wu <cash@cashwu.com>
2018-10-16 22:11:17 +0800

    rename budget to budgetplan

--- src/domain/budgetPlan.js
+++ src/domain/budgetPlan.js
@@ -11,7 +11,7 @@ class Period {
     }
 }
 
-export class Budget {
+export class BudgetPlan {
     budgets = {}
 
     query(startDate, endDate) {
--- tests/unit/domain/budget.spec.js
+++ tests/unit/domain/budget.spec.js
@@ -1,57 +1,55 @@
 import {
-  Budget,
-  getNumbersOfDaysInStartMonth,
-  getNumbersOfDaysInEndMonth
-} from '@/domain/budget'
+  BudgetPlan,
+} from '@/domain/budgetPlan'
 
-describe('Budget', () => {
-  let budget
+describe('BudgetPlan', () => {
+  let budgetPlan
   beforeEach(() => {
-    budget = new Budget()
+    budgetPlan = new BudgetPlan()
   })
   describe('query', () => {
     describe('no budget', () => {
       it('should return 0', () => {
-        expect(budget.query('2018-07-01', '2018-07-31')).toEqual(0)
+        expect(budgetPlan.query('2018-07-01', '2018-07-31')).toEqual(0)
       })
     })
     describe('one budget', () => {
       beforeEach(() => {
-        budget.budgets = {
+        budgetPlan.budgets = {
           '2018-07': 3100
         }
       })
 
       it('within a month', () => {
-        expect(budget.query('2018-07-01', '2018-07-10')).toEqual(1000)
+        expect(budgetPlan.query('2018-07-01', '2018-07-10')).toEqual(1000)
       })
       it('a whole month', () => {
-        expect(budget.query('2018-01-01', '2018-12-31')).toEqual(3100)
+        expect(budgetPlan.query('2018-01-01', '2018-12-31')).toEqual(3100)
       })
       it('query start before the budget', () => {
-        expect(budget.query('2018-06-21', '2018-07-11')).toEqual(1100)
+        expect(budgetPlan.query('2018-06-21', '2018-07-11')).toEqual(1100)
       })
       it('query end before the budget', () => {
-        expect(budget.query('2018-06-21', '2018-06-30')).toEqual(0)
+        expect(budgetPlan.query('2018-06-21', '2018-06-30')).toEqual(0)
       })
       it('query end after the budget', () => {
-        expect(budget.query('2018-07-21', '2018-08-11')).toEqual(1100)
+        expect(budgetPlan.query('2018-07-21', '2018-08-11')).toEqual(1100)
       })
       it('query start after the budget', () => {
-        expect(budget.query('2018-08-21', '2018-09-11')).toEqual(0)
+        expect(budgetPlan.query('2018-08-21', '2018-09-11')).toEqual(0)
       })
     })
     it('query missing budget', () => {
-      budget.budgets = {
+      budgetPlan.budgets = {
         '2018-07': 3100,
         '2018-09': 30000
       }
-      expect(budget.query('2018-07-22', '2018-09-13')).toEqual(1000 + 13000)
+      expect(budgetPlan.query('2018-07-22', '2018-09-13')).toEqual(1000 + 13000)
     })
 
     describe(`given there is budget for 2018-06: 3000, 2018-07: 3100, 2018-08: 3100`, () => {
       beforeEach(() => {
-        budget.budgets = {
+        budgetPlan.budgets = {
           '2018-06': 3000,
           '2018-07': 3100,
           '2018-08': 3100
@@ -60,7 +58,7 @@ describe('Budget', () => {
 
       describe('when query from 2018-06-15 to 2018-08-15', () => {
         it('should return 6200', () => {
-          expect(budget.query('2018-06-15', '2018-08-15')).toEqual(1600 + 3100 + 1500)
+          expect(budgetPlan.query('2018-06-15', '2018-08-15')).toEqual(1600 + 3100 + 1500)
         })
       })
     })
