3cf37ebed515993ddb303b0738d30d14093cd55d
Cash Wu <cash@cashwu.com>
2018-10-16 12:38:06 +0800

    header test

--- tests/unit/components/header.spec.js
+++ tests/unit/components/header.spec.js
@@ -1,15 +1,31 @@
-import Header from '@/components/Header'
+import header from '@/components/Header'
 import {shallowMount} from '@vue/test-utils'
 
 describe('Header', () => {
-  it('hide cake', () => {
-    //Arrange
-    let wrapper = shallowMount(Header)
+    function wrapper() {
+        return shallowMount(header)
+    }
 
-    //Act
-    let profileCaption = wrapper.vm.profileCaption;
+    function wrapperInject(date) {
+        return shallowMount(header, {
+            methods : {
+                getDate() {
+                    return date;
+                }
+            }
+        })
+    }
 
-    //Assert
-    expect(profileCaption).toEqual('Jackson')
-  })
+    it('default value', () => {
+        expect(wrapper().vm.profile).toEqual({name : 'Jackson', birthday : {month : 10, day : 18}})
+    })
+
+    it('not birthday', () => {
+        expect(wrapper().vm.profileCaption).toEqual('Jackson')
+    })
+
+    it('is birthday', () => {
+        var wapper = wrapperInject(new Date(2018, 9, 18))
+        expect(wapper.vm.profileCaption).toEqual('Jackson🎂')
+    })
 })
