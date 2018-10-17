1c6e69bd5014d3e79e05b1e660e341da39e389d4
Jackson Zhang <zbcjackson@gmail.com>
2018-10-17 16:43:38 +0800

    Extract Budget model

--- src/domain/addBudgetPresenter.js
+++ src/domain/addBudgetPresenter.js
@@ -1,6 +1,22 @@
 import Api from "../api";
 import Validator, {FORMAT, NOT_EMPTY, POSITIVE_NUMBER} from "./Validator";
 
+class Budget {
+  constructor({month, amount}){
+    this.month = month
+    this.amount = amount
+  }
+
+  save(){
+    let budgets = Api.getBudgets()
+    let existing = budgets && budgets.find(budget => budget.month === this.month)
+    if (existing) {
+      Api.updateBudget({month: this.month, amount: this.amount})
+    } else {
+      Api.addBudget({month: this.month, amount: this.amount})
+    }
+  }
+}
 export default class AddBudgetPresenter {
   budget = {month: '', amount: 0}
   errors = {month: '', amount: ''}
@@ -10,13 +26,7 @@ export default class AddBudgetPresenter {
   })
   save(success) {
     this.validator.validate(this.budget, () => {
-      let budgets = Api.getBudgets()
-      let existing = budgets && budgets.find(budget => budget.month === this.budget.month)
-      if (existing) {
-        Api.updateBudget(this.budget)
-      } else {
-        Api.addBudget(this.budget)
-      }
+      new Budget(this.budget).save()
       success()
     });
     Object.assign(this.errors, this.validator.errors)
